bcb5242bc0ccf4c24e3656e49d59bb4d15130056
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2021-02-04 14:35:45 +0100

    JsonFile på UserStory

---
+++ UserStoryBoard/UserStoryBoard/Services/JsonFileUserStoryService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using UserStoryBoard.Models;
+
+namespace UserStoryBoard.Services
+{
+    public class JsonFileUserStoryService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public JsonFileUserStoryService(IWebHostEnvironment webHostEnvironment) {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "UserStory.json"); }
+        }
+
+        public IEnumerable<UserStory> GetJsonUserStory() {
+            using (var jsonFileReader = File.OpenText(JsonFileName)) {
+                return JsonSerializer.Deserialize<UserStory[]>(jsonFileReader.ReadToEnd());
+            }
+        }
+
+        public void SaveJsonUserStories(List<UserStory> userStories) 
+        {
+            using (var jsonFileWriter = File.Create(JsonFileName)) {
+                var jsonWriter = new Utf8JsonWriter(jsonFileWriter, new JsonWriterOptions()
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+                JsonSerializer.Serialize<UserStory[]>(jsonWriter, userStories.ToArray());
+            }
+        }
+    }
+}

fafa5e71736d75b52e3b997b5b4aa30e14e20a3a
Caspar Stanley With <casparstan+creative@gmail.com>
2021-02-04 14:26:02 +0100

    Noget der ikke virker men Tob skal bruge

---
+++ UserStoryBoard/UserStoryBoard/Models/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserStoryBoard.Models
+{
+    public class Board
+    {
+        public string Name { get; set; }
+
+        public Board()
+        { }
+
+        public Board(string name)
+        {
+            Name = name;
+        }
+    }
+}

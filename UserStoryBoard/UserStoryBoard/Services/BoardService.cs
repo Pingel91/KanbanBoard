d0544fcd29e3b6c6e28a7aa883a0da37bd9fa21b
Caspar Stanley With <casparstan+creative@gmail.com>
2021-02-05 10:40:49 +0100

    Fix merge conflicts

--- UserStoryBoard/UserStoryBoard/Services/BoardService.cs
+++ UserStoryBoard/UserStoryBoard/Services/BoardService.cs
@@ -48,16 +48,15 @@ namespace UserStoryBoard.Services
             }
         }
 
-<<<<<<< HEAD
         public void AddBoard(Board aBoard)
         {
             kanbanBoards.Add(aBoard);
             // JsonFileUserStoryService.SaveJsonUserStories(kanbanBoards);
-=======
+        }
+
         public void DeleteBoard(Board board)
         {
-            
->>>>>>> 7a772f1c05ac0459b42cc345e247424cce11c148
+
         }
     }
 }

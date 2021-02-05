6535e9dae11d66e17ee2f8304f046a2d5cc7e298
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2021-02-05 14:08:43 +0100

    TimeLine Class m.m. not done!

---
+++ UserStoryBoard/UserStoryBoard/Models/TimeLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserStoryBoard.Models
+{
+    public class TimeLine
+    {
+        public string TitleOfProject { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Sprint> Sprints { get; set; }
+
+        public TimeLine(string titleOfProject, DateTime startDate, DateTime endDate, List<Sprint> sprints)
+        {
+            TitleOfProject = titleOfProject;
+            StartDate = startDate;
+            EndDate = endDate;
+            Sprints = sprints;
+        }
+    }
+}
---
+++ UserStoryBoard/UserStoryBoard/Services/TImeLineServices.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserStoryBoard.MockData;
+using UserStoryBoard.Models;
+
+namespace UserStoryBoard.Services
+{
+    public class TImeLineServices
+    {
+        private List<Sprint> sprints;
+
+        public TImeLineServices()
+        {
+            sprints = MockSprints.GetMockSprints();
+        }
+
+        public List<Sprint> GetAllSprints()
+        {
+            return sprints;
+        }
+
+        public Sprint GetSprint(int id)
+        {
+            foreach (Sprint s in sprints)
+            {
+                if (s.Id == id)
+                    return s;
+            }
+            return null;
+        }
+
+        public void UpdateSprint(Sprint sprint)
+        {
+            if (sprint != null)
+            {
+                for (int s = 0; s < sprints.Count; s++)
+                {
+                    if (sprints[s].Id == sprint.Id)
+                    {
+                        sprints[s] = sprint;
+                    }
+                }
+            }
+        }
+
+        public void AddSprint(Sprint aSprint)
+        {
+            sprints.Add(aSprint);
+        }
+
+    }
+}

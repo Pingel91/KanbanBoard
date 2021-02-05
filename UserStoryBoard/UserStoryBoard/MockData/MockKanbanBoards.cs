c382693bf891ac07e86f0b8669e273cf64e0e78f
DESKTOP-PV67OOQ\tobia <tobi4504@edu.easj.dk>
2021-02-05 09:30:08 +0100

    Delete + MockUP

---
+++ UserStoryBoard/UserStoryBoard/MockData/MockKanbanBoards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserStoryBoard.Models;
+
+namespace UserStoryBoard.MockData
+{
+    public class MockKanbanBoards
+    {
+
+        public static List<Board> kanbanBoards = new List<Board>()
+        {
+            new Board("Kanban Board 1"),
+            new Board("Caspars Test Board"),
+            new Board("Board")
+        };
+
+
+        public static List<Board> GetMockBoards()
+        {
+            return kanbanBoards;
+        }
+    }
+}

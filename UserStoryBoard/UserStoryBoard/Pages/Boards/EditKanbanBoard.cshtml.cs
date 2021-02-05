93113664794a845d554eeb5300ac2553464d12a9
Caspar Stanley With <casparstan+creative@gmail.com>
2021-02-05 09:58:41 +0100

    Edit Kanban Board WIP

---
+++ UserStoryBoard/UserStoryBoard/Pages/Boards/EditKanbanBoard.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UserStoryBoard.Models;
+using UserStoryBoard.Services;
+
+namespace UserStoryBoard.Pages.Boards
+{
+    public class EditKanbanBoardModel : PageModel
+    {
+        private BoardService boardService;
+
+        [BindProperty]
+        public UserStory UserStory { get; set; }
+
+        public EditKanbanBoardModel(BoardService bService)
+        {
+            this.boardService = bService;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            KanbanBoard = boardService.GetBoard(id);
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            boardService.UpdateBoard(KanbanBoard);
+
+            return RedirectToPage("UserStories");
+        }
+    }
+}

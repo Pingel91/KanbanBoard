6503cad92e3950fe8d94fb659243c3f857d0605f
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2021-02-06 16:52:47 +0100

    :-)

--- UserStoryBoard/UserStoryBoard/Models/Sprint.cs
+++ UserStoryBoard/UserStoryBoard/Models/Sprint.cs
@@ -7,5 +7,19 @@ namespace UserStoryBoard.Models
 {
     public class Sprint
     {
+        public int Id { get; set; }
+        public string SprintName { get; set; }
+        public DateTime SprintStart { get; set; }
+        public DateTime SprintEnd { get; set; }
+        public static int nextId = 1;
+
+        public Sprint(string sprintName, DateTime sprintStart, DateTime sprintEnd)
+        {
+            Id = nextId++;
+            SprintName = sprintName;
+            SprintStart = sprintStart;
+            SprintEnd = sprintEnd;
+        }
+
     }
 }

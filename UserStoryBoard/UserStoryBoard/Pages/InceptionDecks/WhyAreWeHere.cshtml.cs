dae7a185a3854fe85dd9c42cde78eb243476685e
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2021-02-10 13:23:54 +0100

    Mere inceptions deck - added alle 10 punkter

---
+++ UserStoryBoard/UserStoryBoard/Pages/InceptionDecks/WhyAreWeHere.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserStoryBoard.Pages.InceptionDecks
+{
+    public class WhyAreWeHereModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

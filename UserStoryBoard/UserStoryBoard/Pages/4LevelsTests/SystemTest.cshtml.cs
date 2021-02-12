92ba658fbc93d63ca8c7d1995d242e7dca78bd48
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2021-02-12 10:43:41 +0100

    SystemTest prework

---
+++ UserStoryBoard/UserStoryBoard/Pages/4LevelsTests/SystemTest.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserStoryBoard.Pages._4LevelsTests
+{
+    public class SystemTestModel : PageModel
+    {
+        public int SystemTests;
+        
+        public void OnGet()
+        {
+        }
+    }
+}

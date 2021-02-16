b770dd65d3b0cced62dbc029d2aa642e9539c44d
Caspar Stanley With <casparstan+creative@gmail.com>
2021-02-16 13:32:18 +0100

    Added Login / authentication - not working fully yet, but neat nonetheless

---
+++ UserStoryBoard/UserStoryBoard/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserStoryBoard.Data;
+
+[assembly: HostingStartup(typeof(UserStoryBoard.Areas.Identity.IdentityHostingStartup))]
+namespace UserStoryBoard.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserStoryBoardContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserStoryBoardContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserStoryBoardContext>();
+            });
+        }
+    }
+}
--- UserStoryBoard/UserStoryBoard/Pages/4LevelsTests/SystemTest.cshtml.cs
+++ UserStoryBoard/UserStoryBoard/Pages/4LevelsTests/SystemTest.cshtml.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UserStoryBoard.Models;
 
 namespace UserStoryBoard.Pages._4LevelsTests
 {
     public class SystemTestModel : PageModel
     {
-        public int SystemTests;
+        public List<Test> SystemTests { get; private set; }
+
         
         public void OnGet()
         {
+            //SystemTests = TestsService.GetTests();
         }
     }
 }

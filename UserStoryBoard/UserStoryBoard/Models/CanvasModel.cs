aede1860445071e6e373f46eb3940701efd92d3c
pnyskjol <patr447g@edu.easj.dk>
2021-02-18 17:12:34 +0100

    added canvasmodel and swotanalysis class - to be worked on tomorrow

---
+++ UserStoryBoard/UserStoryBoard/Models/CanvasModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserStoryBoard.Models
+{
+    public class CanvasModel
+    {
+        public string KeyPartners { get; set; }
+        public string KeyActivities { get; set; }
+        public string KeyResources { get; set; }
+        public string ValueProposition { get; set; }
+        public string CustomerRelationship { get; set; }
+        public string Channels { get; set; }
+        public string CustomerSegment { get; set; }
+        public string CostStructure { get; set; }
+        public string RevenueStreams { get; set; }
+
+        public CanvasModel()
+        {
+
+        }
+
+        public CanvasModel(string keyPartners, string keyActivities, string keyResources, string valueProposition, string customerRelationship, string channels, string customerSegment, string costStructure, string revenueStreams)
+        {
+            KeyPartners = keyPartners;
+            KeyActivities = keyActivities;
+            KeyResources = keyResources;
+            ValueProposition = valueProposition;
+            CustomerRelationship = customerRelationship;
+            Channels = channels;
+            CustomerSegment = customerSegment;
+            CostStructure = costStructure;
+            RevenueStreams = revenueStreams;
+        }
+    }
+}
---
+++ UserStoryBoard/UserStoryBoard/Models/SWOTAnalysis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserStoryBoard.Models
+{
+    public class SWOTAnalysis
+    {
+        public string Strengths { get; set; }
+        public string Weaknesses { get; set; }
+        public string Opportunity { get; set; }
+        public string Threats { get; set; }
+
+        public SWOTAnalysis()
+        {
+
+        }
+
+        public SWOTAnalysis(string strengths, string weaknesses, string opportunity, string threats)
+        {
+            Strengths = strengths;
+            Weaknesses = weaknesses;
+            Opportunity = opportunity;
+            Threats = threats;
+        }
+    }
+}

2ab004addc55128203e3cc8a51c5b45296d25809
Caspar Stanley With <casparstan+creative@gmail.com>
2021-03-19 11:17:47 +0100

    Fixed the Azure error maybe?

--- UserStoryBoard/UserStoryBoard/Startup.cs
+++ UserStoryBoard/UserStoryBoard/Startup.cs
@@ -60,14 +60,18 @@ namespace UserStoryBoard
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
-            else {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
+            app.UseDeveloperExceptionPage();
+            // Commented this out to avoid Azure error
+            //if (env.IsDevelopment()) {
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else {
+            //    // This is what is created with the project
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
